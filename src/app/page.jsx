a2417bc2c92de9ae6f29e43b624b1f86670449b7
Saxboyy <abelfalconespino@gmail.com>
2024-05-24 20:36:03 +0100

    fix: Now, can be buiilded

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { Suspense, useState, useEffect } from "react";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 import { Twitter, Linkedin, Github } from "lucide-react";
 import profilePic from "./images/avatar.jpeg";
 import About from "./pages/about";
 import { useSearchParams, useRouter } from "next/navigation";
 
-export default function Home() {
+function Content() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const [tab, setTab] = useState(searchParams.get("tab") || "about");
@@ -27,10 +27,106 @@ export default function Home() {
       case "about":
         return <About />;
       default:
-        return <div>Ups, algo esta yendo mal...</div>;
+        return <div>Ups, algo está yendo mal...</div>;
     }
   };
 
+  return (
+    <div>
+      <div className="flex items-center flex-wrap gap-2 mb-8">
+        <a href="/?tab=about" shallow="true">
+          <button
+            className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
+              tab === "about"
+                ? "text-white"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            <span className="relative z-10">Sobre mi</span>
+            <span
+              className={`absolute inset-0 z-0 rounded-md ${
+                tab === "about" ? "bg-gray-900" : ""
+              }`}
+            ></span>
+          </button>
+        </a>
+        <a href="/?tab=blog" shallow="true">
+          <button
+            className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
+              tab === "blog"
+                ? "text-white"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            <span className="relative z-10">Blogs</span>
+            <span
+              className={`absolute inset-0 z-0 rounded-md ${
+                tab === "blog" ? "bg-gray-900" : ""
+              }`}
+            ></span>
+          </button>
+        </a>
+        <a href="/?tab=uses" shallow="true">
+          <button
+            className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
+              tab === "uses"
+                ? "text-white"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            <span className="relative z-10">Uses</span>
+            <span
+              className={`absolute inset-0 z-0 rounded-md ${
+                tab === "uses" ? "bg-gray-900" : ""
+              }`}
+            ></span>
+          </button>
+        </a>
+        <a href="/?tab=newsletter" shallow="true">
+          <button
+            className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
+              tab === "newsletter"
+                ? "text-white"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            <span className="relative z-10">Newsletter</span>
+            <span
+              className={`absolute inset-0 z-0 rounded-md ${
+                tab === "newsletter" ? "bg-gray-900" : ""
+              }`}
+            ></span>
+          </button>
+        </a>
+        <a href="/?tab=contact" shallow="true">
+          <button
+            className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
+              tab === "contact"
+                ? "text-white"
+                : "text-gray-500 hover:text-gray-900"
+            }`}
+          >
+            <span className="relative z-10">Contacto</span>
+            <span
+              className={`absolute inset-0 z-0 rounded-md ${
+                tab === "contact" ? "bg-gray-900" : ""
+              }`}
+            ></span>
+          </button>
+        </a>
+      </div>
+      <div
+        className={`transition-opacity duration-300 ${
+          tab !== "about" && "opacity-0"
+        }`}
+      >
+        {renderContent()}
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
   return (
     <main className="flex items-center justify-between w-full flex-col p-8 min-h-screen">
       <div className="w-full max-w-3xl">
@@ -42,7 +138,6 @@ export default function Home() {
               width="50"
               height="50"
               decoding="async"
-              className="rounded-full"
               src={profilePic}
             />
           </a>
@@ -75,89 +170,9 @@ export default function Home() {
           </div>
         </div>
         <div className="border-b w-full my-8"></div>
-        <div className="flex items-center flex-wrap gap-2 mb-8">
-          <a href="/?tab=about" shallow="true">
-            <button
-              className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
-                tab === "about"
-                  ? "text-white"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-            >
-              <span className="relative z-10">Sobre mi</span>
-              <span
-                className={`absolute inset-0 z-0 rounded-md ${
-                  tab === "about" ? "bg-gray-900" : ""
-                }`}
-              ></span>
-            </button>
-          </a>
-          <a href="/?tab=blog" shallow="true">
-            <button
-              className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
-                tab === "blog"
-                  ? "text-white"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-            >
-              <span className="relative z-10">Blogs</span>
-              <span
-                className={`absolute inset-0 z-0 rounded-md ${
-                  tab === "blog" ? "bg-gray-900" : ""
-                }`}
-              ></span>
-            </button>
-          </a>
-          <a href="/?tab=uses" shallow="true">
-            <button
-              className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
-                tab === "uses"
-                  ? "text-white"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-            >
-              <span className="relative z-10">Uses</span>
-              <span
-                className={`absolute inset-0 z-0 rounded-md ${
-                  tab === "uses" ? "bg-gray-900" : ""
-                }`}
-              ></span>
-            </button>
-          </a>
-          <a href="/?tab=newsletter" shallow="true">
-            <button
-              className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
-                tab === "newsletter"
-                  ? "text-white"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-            >
-              <span className="relative z-10">Newsletter</span>
-              <span
-                className={`absolute inset-0 z-0 rounded-md ${
-                  tab === "newsletter" ? "bg-gray-900" : ""
-                }`}
-              ></span>
-            </button>
-          </a>
-          <a href="/?tab=contact" shallow="true">
-            <button
-              className={`text-sm transition-colors px-2 py-1 rounded-md relative ${
-                tab === "contact"
-                  ? "text-white"
-                  : "text-gray-500 hover:text-gray-900"
-              }`}
-            >
-              <span className="relative z-10">Contacto</span>
-              <span
-                className={`absolute inset-0 z-0 rounded-md ${
-                  tab === "contact" ? "bg-gray-900" : ""
-                }`}
-              ></span>
-            </button>
-          </a>
-        </div>
-        <div>{renderContent()}</div>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Content />
+        </Suspense>
       </div>
     </main>
   );
